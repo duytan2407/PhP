fdbf7a67a6f00b64c580193b25b9634e987e7095
duytan2407 <nguyendam554@gmail.com>
2022-04-08 21:42:13 +0700

    aa

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $table = "products";
+
+   
+
+    public function product_type(){
+        return $this->belongsTo('App\Models\Product','id_type','id');
+    }
+
+    public function bill_detail(){
+        return $this->hasMany('App\Models\Product','id_product','id');
+    }
+}
